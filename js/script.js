c739c27221f10a99bc454924eb050edbe7f9b1bc
Stella-Chaves <stellachavesyoga@gmail.com>
2018-08-31 17:05:34 -0500

    NYT AJAX request

--- js/script.js
+++ js/script.js
@@ -16,12 +16,30 @@ function loadData() {
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
 
-    $greeting.text('Life at' + address + '.')
+    $greeting.text('Live at' + address + '.')
 
     var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +address + '';
     $body.append('<img class="bgimg" src= "' + streetviewUrl + '" >');
 
     return false;
-};
+
+    // NY Times AJAX request
+    var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=15e12821ebdc4cadb63df644fb557d8e'
+    
+    $.getJSON(nytimesURL, function(data) {
+        
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+          var article = articles[i];
+          $nytElem.append('<li class="article">'+
+              '<a href="'+article.web_url+'">'+article.headline.main+
+                  '</a>'+
+              '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+  })
+}
 
 $('#form-container').submit(loadData);
